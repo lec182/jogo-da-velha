dfb9a046e0d5ad12c41b94d847a7b4cf79cd3365
Lucas Cardoso <lucas.emanuel.cardoso@hotmail.com>
2023-03-04 11:52:57 -0300

    Deploy via pasta docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { JogoDaVelhaModule } from './jogo-da-velha';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    JogoDaVelhaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
